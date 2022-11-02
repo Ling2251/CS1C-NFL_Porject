356c411663bceb03611a14ef1eb363214f5be9b3
Peijun Zhao <pzhao1@saddleback.edu>
2022-11-02 13:57:42 -0700

    Basci layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):
+    QMainWindow(parent) ,
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Gave some of the buttom icon
+    ui->HelpButton->setIcon(QIcon(":/Images/Help.jpg"));
+    ui->ContactUsButton->setIcon(QIcon(":/Images/Contact us.png"));
 }
 
 MainWindow::~MainWindow()
