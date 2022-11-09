dd25e2bb30decb6036a651fb6e3c9e2ecd4b04a5
Maya <mayabee290179@gmail.com>
2022-11-09 15:10:26 -0800

    added everyting to display by name

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,4 @@
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "contact.h"
@@ -58,34 +59,48 @@ void MainWindow::on_HelpButton_clicked()
 void MainWindow::on_TeamName_clicked()
 {
     ui->teamWidget->setRowCount(32);
-    ui->teamWidget->setColumnCount(1);
+    ui->teamWidget->setColumnCount(9);
     ui->teamWidget->setColumnWidth(0,200);
+
     int i, j, arraysize = 32;
 
     NFLInput temp;
 
+    // sort by team name
     for(i = 0; i<arraysize; i++) {
-
-      for(j = i+1; j<arraysize; j++)
-
-      {
-          if(arr[j].getTeamName() < arr[i].getTeamName()) {
-
-            temp = arr[i];
-
-            arr[i] = arr[j];
-
-            arr[j] = temp;
-
-          }
-      }
+        for(j = i+1; j<arraysize; j++)
+        {
+            if(arr[j].getTeamName() < arr[i].getTeamName()) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
 
-    for (int i=0; i<arraysize; i++) {
-        ui->teamWidget->setItem(0,i,  new QTableWidgetItem(arr[i].getTeamName().c_str())) ;
+    // print the table headers
+    ui->teamWidget->setItem(0,0, new QTableWidgetItem("Team Name"));
+    ui->teamWidget->setItem(0,1, new QTableWidgetItem("Stadium Name"));
+    ui->teamWidget->setItem(0,2, new QTableWidgetItem("Seating Capacity"));
+    ui->teamWidget->setItem(0,3, new QTableWidgetItem("Location"));
+    ui->teamWidget->setItem(0,4, new QTableWidgetItem("Conference"));
+    ui->teamWidget->setItem(0,5, new QTableWidgetItem("Division"));
+    ui->teamWidget->setItem(0,6, new QTableWidgetItem("Surface Type"));
+    ui->teamWidget->setItem(0,7, new QTableWidgetItem("Stadium Roof Type"));
+    ui->teamWidget->setItem(0,8, new QTableWidgetItem("Date Opened"));
+
+    // fill out the table
+    for (i=0; i<arraysize; i++) {
+        ui->teamWidget->setItem(i, 0, new QTableWidgetItem(arr[i].getTeamName().c_str()));
+        ui->teamWidget->setItem(i, 1, new QTableWidgetItem(arr[i].getStadiumName().c_str()));
+        ui->teamWidget->setItem(i, 2, new QTableWidgetItem(to_string(arr[i].getSeatingCapacity()).c_str()));
+        ui->teamWidget->setItem(i, 3, new QTableWidgetItem(arr[i].getLocation().c_str()));
+        ui->teamWidget->setItem(i, 4, new QTableWidgetItem(arr[i].getConference().c_str()));
+        ui->teamWidget->setItem(i, 5, new QTableWidgetItem(arr[i].getDivision().c_str()));
+        ui->teamWidget->setItem(i, 6, new QTableWidgetItem(arr[i].getSurfaceType().c_str()));
+        ui->teamWidget->setItem(i, 7, new QTableWidgetItem(arr[i].getStadiumRoofType().c_str()));
+        ui->teamWidget->setItem(i, 8, new QTableWidgetItem(to_string(arr[i].getDateOpened()).c_str()));
     }
+
 }
-
-
-
 
